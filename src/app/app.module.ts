08d5c362be943220700834fb1cfdad9af2c8f7a9
eugenioenko <eugenioenko@gmail.com>
2019-07-26 15:00:11 -0300

    updated angular version. reorganized project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-
 import { AppComponent } from './app.component';
-import { MonthViewComponent } from './month-view/month-view.component';
-import { MonthSelectorComponent } from './month-selector/month-selector.component';
-import { DayViewComponent } from './day-view/day-view.component';
-import { CalendarViewComponent } from './calendar-view/calendar-view.component';
-
+import { MonthViewComponent } from './components/month-view/month-view.component';
+import { MonthSelectorComponent } from './components/month-selector/month-selector.component';
+import { DayViewComponent } from './components/day-view/day-view.component';
+import { CalendarViewComponent } from './components/calendar-view/calendar-view.component';
 
 @NgModule({
   declarations: [
